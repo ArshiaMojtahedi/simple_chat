0fd875cdb666598f9766895c267a63759d0f1f09
Arshia Mojtahedi <arshia.mojtahedi@zamann-pharma.com>
2023-05-30 18:48:40 +0330

    Chat list and details cubit and state added

--- lib/Presentation/chat_details/cubit/chat_details_cubit.dart
+++ lib/Presentation/chat_details/cubit/chat_details_cubit.dart
@@ -1,8 +1,23 @@
 import 'package:bloc/bloc.dart';
 import 'package:meta/meta.dart';
+import 'package:simple_chat/Data/models/message_model.dart';
+
+import '../../../Domain/usecases/get_chat_details_usecase.dart';
 
 part 'chat_details_state.dart';
 
 class ChatDetailsCubit extends Cubit<ChatDetailsState> {
-  ChatDetailsCubit() : super(ChatDetailsInitial());
+  final GetChatDetailsUseCase getChatDetailsUseCase;
+
+  ChatDetailsCubit({required this.getChatDetailsUseCase})
+      : super(ChatDetailsInitial());
+
+  Future<void> fetchChatDetails(String chatId) async {
+    emit(ChatDetailsLoading());
+    final result = await getChatDetailsUseCase(chatId);
+    result.fold(
+      (exception) => emit(ChatDetailsError(exception.toString())),
+      (messages) => emit(ChatDetailsLoaded(messages)),
+    );
+  }
 }
